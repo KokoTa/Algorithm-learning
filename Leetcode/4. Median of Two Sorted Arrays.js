6a53e120ca9870f04415e07f04d55aa7ec1b071c
KokoTa <584847514@qq.com>
2017-09-23 20:57:51 +0800

    Median of Two Sorted Arrays

---
+++ Leetcode/4. Median of Two Sorted Arrays.js
@@ -0,0 +1,34 @@
+// There are two sorted arrays nums1 and nums2 of size m and n respectively.
+// Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+// Example 1:
+// nums1 = [1, 3]
+// nums2 = [2]
+// The median is 2.0
+
+// Example 2:
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+// The median is (2 + 3)/2 = 2.5
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+	var mid = 0;
+	var arr = nums1.concat(nums2);
+	arr.sort((a, b) => a - b);
+	var len = arr.length;
+
+	if(len % 2 === 0) {
+		mid = ((arr[(len/2)-1]) + (arr[len/2])) / 2; // Look!这里(() + ())可以优化性能噢！
+	} else {
+		mid = arr[Math.floor(len/2)];
+	}
+
+	return mid;
+};
+
+console.log(findMedianSortedArrays([1,2],[3,4]))
